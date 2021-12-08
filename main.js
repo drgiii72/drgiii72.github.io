ebf4aab8d65786f307e5a8896f27c1e176d09518
drgiii72 <64932916+drgiii72@users.noreply.github.com>
2021-12-08 16:15:09 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,137 @@
+
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+
+/*show menu*/
+if(navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu');
+    })
+}
+
+/*hide menu*/
+if (navClose) {
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu');
+    })
+}
+
+
+
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu')
+
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+
+
+
+
+
+
+
+
+/*Scroll Sections Active Link*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive() {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop= current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        } else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+
+
+
+
+
+
+
+/*Change Background Header*/
+function scrollHeader() {
+    const nav = document.getElementById('header')
+
+    if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove(scroll-header)
+}
+window.addEventListener('scroll', scrollHeader)
+
+
+
+
+/*Scrollup*/
+function scrollUp() {
+    const scrollUp = document.getElementById('scroll-up');
+    if (this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll');
+}
+window.addEventListener('scroll', scrollUp)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*Portfolio Swiper*/
+let swiper = new Swiper(".portfolio__container", {
+    cssMode: true,
+    loop: true,
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
